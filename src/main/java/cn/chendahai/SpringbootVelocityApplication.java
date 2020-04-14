050cfa9b500410f04a19bd878b21579a62709c07
陈海洋 <1559843332@qq.com>
2020-04-14 11:08:42 +0800

    自定义404页面访问异常

--- src/main/java/cn/chendahai/SpringbootVelocityApplication.java
+++ src/main/java/cn/chendahai/SpringbootVelocityApplication.java
@@ -2,12 +2,26 @@ package cn.chendahai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
+//exclude = ErrorMvcAutoConfiguration.class
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@Controller
 public class SpringbootVelocityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootVelocityApplication.class, args);
     }
 
+    /**
+     * 自定义404页面访问异常
+     * 需要exclude = ErrorMvcAutoConfiguration.class
+     * 否则会自动访问/error,跳转到BasicErrorController
+     */
+    @RequestMapping("/**")
+    public String notFound() {
+        return "404";
+    }
 }
